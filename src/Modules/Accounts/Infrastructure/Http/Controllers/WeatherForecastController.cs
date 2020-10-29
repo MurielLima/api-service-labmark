959b20863620c4f9dea9b0acd7c526dd88434aa9
Muriel.Lima <murieljlima@gmail.com>
2020-10-29 00:49:14 -0300

    t

--- src/Modules/Accounts/Infrastructure/Http/Controllers/WeatherForecastController.cs
+++ src/Modules/Accounts/Infrastructure/Http/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Labmark.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "", "", "", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "", "", "", "Cool", "", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
