9ba3e9b8dc38f1a960cb81889d353dc51fae36ae
Muriel.Lima <murieljlima@gmail.com>
2021-01-12 20:43:22 -0300

    exemplo implementation controller

--- Src/api-application-labmark/Domain/Modules/Solicitation/Infrastructure/Controllers/SolicitationController.cs
+++ Src/api-application-labmark/Domain/Modules/Solicitation/Infrastructure/Controllers/SolicitationController.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Labmark.Domain.Modules.Sample.Controllers;
+using Labmark.Domain.Modules.Solicitation.Infrastructure.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Labmark.Domain.Modules.Solicitation.Infrastructure.Controllers
 {
     public class SolicitationController : ISolicitationController
     {
+        public Task<IActionResult> Create([FromBody] SolicitationDto solicitationDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> List([FromRoute] int? solicitationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Update([FromBody] SolicitationDto solicitationDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
