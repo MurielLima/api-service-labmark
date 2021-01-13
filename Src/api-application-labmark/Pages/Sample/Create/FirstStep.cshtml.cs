974b4bac928637e1b2116722f9f5b3366698c071
Muriel.Lima <murieljlima@gmail.com>
2021-01-12 23:05:49 -0300

    Exemplo de dropdownlist

--- Src/api-application-labmark/Pages/Sample/Create/FirstStep.cshtml.cs
+++ Src/api-application-labmark/Pages/Sample/Create/FirstStep.cshtml.cs
@@ -1,15 +1,28 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Labmark.Domain.Modules.Client.Controllers;
+using Labmark.Domain.Modules.Client.Infrastructure.Models.Dtos;
 using Labmark.Domain.Modules.Solicitation.Infrastructure.Models.Dtos;
 using Labmark.Domain.Modules.Solicitation.Infrastructure.Models.Enums;
+using Labmark.Domain.Shared.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Labmark.Pages.Sample.Create
 {
     public class FirstStepModel : PageModel
     {
+        private readonly IClientController _clientController;
+        public FirstStepModel(IClientController clientController)
+        {
+            _clientController = clientController;
+        }
+
         public IActionResult OnGet()
         {
+            ResponseDto responseDto = (ResponseDto)((ObjectResult) _clientController.List(_selectedClientId).Result).Value;
+            _clientDtos = (List<ClientDto>)responseDto.detail;
             return Page();
         }
         public IActionResult OnPost()
@@ -25,8 +38,12 @@ namespace Labmark.Pages.Sample.Create
             _solicitationDto.AskDtos.Add(_volume);
             _solicitationDto.AskDtos.Add(_temperature);
             _solicitationDto.AskDtos.Add(_transport);
-            return Page();
+            return Redirect($"/Sample/SecondStep/?solicitationId={_solicitationDto.Id}");
         }
+        [BindProperty]
+        public IList<ClientDto> _clientDtos { get; set; }
+        [BindProperty]
+        public int _selectedClientId { get; set; }
         [BindProperty]
         public SolicitationDto _solicitationDto { get; set; }
         [BindProperty]
---
+++ Src/api-application-labmark/Pages/Sample/Create/TechnicalManagerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Labmark.Pages.Sample.Create
+{
+    public class TechnicalManagerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
