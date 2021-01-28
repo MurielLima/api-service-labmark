3d0ca3b50cf695d1dde6bcb19f91a73b9e1addfb
Luis Gabriel Kingeski <luisgkingeski@outlook.com>
2021-01-28 20:14:52 -0300

    Foreach para WaterDilution e modificaçoes no EnumWaterDilution

--- Src/api-application-labmark/Domain/Modules/Sample/Infrastructure/Models/Dtos/WaterDilutionDto.cs
+++ Src/api-application-labmark/Domain/Modules/Sample/Infrastructure/Models/Dtos/WaterDilutionDto.cs
@@ -4,6 +4,12 @@ namespace Labmark.Domain.Modules.Sample.Infrastructure.Models.Dtos
 {
     public class WaterDilutionDto
     {
+        public WaterDilutionDto(EnumWaterDilution enumWaterDilution)
+        {
+            Code = enumWaterDilution;
+        }
+
+
         public int Id { get; set; }
         public DilutionSampleDto DilutionSample { get; set; }
         public EnumWaterDilution Code { get; set; }
--- Src/api-application-labmark/Domain/Modules/Sample/Infrastructure/Models/Enums/EnumWaterDilution.cs
+++ Src/api-application-labmark/Domain/Modules/Sample/Infrastructure/Models/Enums/EnumWaterDilution.cs
@@ -1,6 +1,14 @@
-﻿namespace Labmark.Domain.Modules.Sample.Infrastructure.Models.Enums
+﻿using System.ComponentModel;
+
+namespace Labmark.Domain.Modules.Sample.Infrastructure.Models.Enums
 {
     public enum EnumWaterDilution
     {
+        [Description("Agua-Diluição 225ml")]
+        ML225 = 1,
+        [Description("Agua-Diluição 9ml")]
+        ML9 = 2
+
+
     }
 }
