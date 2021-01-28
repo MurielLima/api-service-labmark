81f6c97dce7c8d215b908a0315381e4803b259e2
Luis Gabriel Kingeski <luisgkingeski@outlook.com>
2021-01-28 17:42:41 -0300

    Adicionado EnumPoints e alteraçoes no FirstStep do SampleDilution

---
+++ Src/api-application-labmark/Domain/Modules/Exam/Infrastructure/Models/Enums/EnumPoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labmark.Domain.Modules.Exam.Infrastructure.Models.Enums
+{
+    public enum EnumPoints
+    {
+        [Description("1")]
+        P1 = 1,
+        [Description("2")]
+        P2 = 2,
+        [Description("3")]
+        P3 = 3
+
+    }
+}
--- Src/api-application-labmark/Domain/Modules/Sample/Infrastructure/Models/Dtos/PointDto.cs
+++ Src/api-application-labmark/Domain/Modules/Sample/Infrastructure/Models/Dtos/PointDto.cs
@@ -1,11 +1,24 @@
-﻿namespace Labmark.Domain.Modules.Sample.Infrastructure.Models.Dtos
+﻿using Labmark.Domain.Modules.Exam.Infrastructure.Models.Enums;
+
+namespace Labmark.Domain.Modules.Sample.Infrastructure.Models.Dtos
 {
     public class PointDto
     {
+        public PointDto()
+        {
+
+        }
+        public PointDto(EnumPoints enumPoints)
+        {
+            Code = enumPoints;
+        }
+
         public int Id { get; set; }
         public DilutionSampleDto DilutionSample { get; set; }
         public double Middle { get; set; }
         public int BOD { get; set; }
         public string Lot { get; set; }
+
+        public EnumPoints Code { get; set; }
     }
 }
