323c0c02b1dd7e9fd9082743410059f6d7910e3d
Luis Gabriel Kingeski <luisgkingeski@outlook.com>
2021-01-31 18:34:48 -0300

    criaçao Mappers

---
+++ Src/api-application-labmark/Domain/Modules/Exam/Infrastructure/Mappers/ColiformesEscherichiaMapToColiformsEscherichiaDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Labmark.Domain.Modules.Exam.Infrastructure.EFCore.Entities;
+using Labmark.Domain.Modules.Exam.Infrastructure.Models.Dtos;
+
+namespace Labmark.Domain.Modules.Exam.Infrastructure.Mappers
+{
+    public class ColiformesEscherichiaMapToColiformsEscherichiaDto
+    {
+
+        public static ColiformsEscherichiaDto Map(ColiformsEscherichiaDto coliformsEscherichiaDto, ColiformesEscherichia coliformesEscherichia)
+        {
+            coliformsEscherichiaDto.BOD = coliformesEscherichia.BOD ?? 0;
+            coliformsEscherichiaDto.Brilla = coliformesEscherichia.Brilla;
+            coliformsEscherichiaDto.DateOfCompletion = coliformesEscherichia.DataResultado;
+            coliformsEscherichiaDto.DateResult = coliformesEscherichia.DataResultado;
+            coliformsEscherichiaDto.Escherichia = coliformesEscherichia.Escherichia;
+            
+
+
+
+
+            return null;
+        }
+
+
+
+
+
+
+
+
+    }
+}
--- Src/api-application-labmark/Domain/Modules/Exam/Infrastructure/Models/Dtos/ColiformsEscherichiaDto.cs
+++ Src/api-application-labmark/Domain/Modules/Exam/Infrastructure/Models/Dtos/ColiformsEscherichiaDto.cs
@@ -6,17 +6,17 @@ namespace Labmark.Domain.Modules.Exam.Infrastructure.Models.Dtos
     public class ColiformsEscherichiaDto
     {
         public int Point { get; set; }
-        public string Brilla { get; set; }
+        public double? Brilla { get; set; }
         public int BOD { get; set; }
         public int WatherBath { get; set; }
         public int FlowMicropipettor { get; set; }
-        public string Escherichia { get; set; }
+        public double? Escherichia { get; set; }
         public string TotalColifoms { get; set; }
         public string TolerantColiforms { get; set; }
-        public DateTime DateOfCompletion { get; set; }
+        public DateTime? DateOfCompletion { get; set; }
         public string Result { get; set; }
-        public DateTime DateTime { get; set; }
-        public SampleTestDto EssayBySample { get; set; }
+        public DateTime? DateResult { get; set; }
+        
 
     }
 }
