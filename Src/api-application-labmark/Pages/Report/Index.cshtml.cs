ad3178dd03ad86f293285cd825bd39c8dc307180
Muriel.Lima <murieljlima@gmail.com>
2021-01-31 00:03:46 -0300

    dropdown

--- Src/api-application-labmark/Pages/Report/Index.cshtml.cs
+++ Src/api-application-labmark/Pages/Report/Index.cshtml.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Labmark.Domain.Modules.Client.Infrastructure.Models.Dtos;
+using Labmark.Domain.Modules.Sample.Infrastructure.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +8,10 @@ namespace Labmark.Pages.Report
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public IList<SampleDto> _sampleDtos { get; set; }
+        [BindProperty]
+        public SampleDto _sampleDto { get; set; }
         public IActionResult OnGet()
         {
             return Page();
