079a76d4a586555efef9a14f54b69876eeab6c74
Muriel José Lima <murieljlima@gmail.com>
2021-02-02 23:30:49 -0300

    ensaio repository

--- Src/api-application-labmark/Domain/Modules/Sample/Infrastructure/EFCore/Repositories/EnsaioRepository.cs
+++ Src/api-application-labmark/Domain/Modules/Sample/Infrastructure/EFCore/Repositories/EnsaioRepository.cs
@@ -5,11 +5,16 @@ using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Labmark.Domain.Modules.Sample.Infrastructure.EFCore.Entities;
 using Labmark.Domain.Modules.Sample.Repositories;
+using Labmark.Domain.Shared.Infrastructure.EFCore;
+using Labmark.Domain.Shared.Infrastructure.EFCore.Repositories;
 
 namespace Labmark.Domain.Modules.Sample.Infrastructure.EFCore.Repositories
 {
-    public class EnsaioRepository : IEnsaioRepository
+    public class EnsaioRepository : Repository<Ensaio>, IEnsaioRepository
     {
-        
+        public EnsaioRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
     }
 }
