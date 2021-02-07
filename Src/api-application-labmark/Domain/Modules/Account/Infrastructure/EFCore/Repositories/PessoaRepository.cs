8d6190e49138c8960818525d3fb13bb3d3637306
Muriel José Lima <murieljlima@gmail.com>
2021-02-07 04:56:31 -0300

    ajustes

--- Src/api-application-labmark/Domain/Modules/Account/Infrastructure/EFCore/Repositories/PessoaRepository.cs
+++ Src/api-application-labmark/Domain/Modules/Account/Infrastructure/EFCore/Repositories/PessoaRepository.cs
@@ -14,24 +14,24 @@ namespace Labmark.Domain.Modules.Account.Infrastructure.EFCore.Repositories
         }
         public async Task<Pessoa> FindByEmail(string email)
         {
-            return await dbSet.FindAsync(new { Email = email, TipoAcesso = "U" });
+            return await dbSet.FindAsync(new { Email = email, TipoAcesso = 'U' });
         }
         public async Task<IList<Pessoa>> ListAllUsers()
         {
-            return await Get(x => x.TipoAcesso.Equals("U"));
+            return await Get(x => x.TipoAcesso.Equals('U'));
         }
         public async override Task<Pessoa> GetByID(int id)
         {
-            return await dbSet.FindAsync(new { Id = id, TipoAcesso = "U" });
+            return await dbSet.FindAsync(new Pessoa{ Id = id, TipoAcesso = 'U' });
         }
         public override bool Save(Pessoa entity)
         {
-            entity.TipoAcesso = "U";
+            entity.TipoAcesso = 'U';
             return base.Save(entity);
         }
         public override bool Insert(Pessoa entity)
         {
-            entity.TipoAcesso = "U";
+            entity.TipoAcesso = 'U';
             return base.Insert(entity);
         }
     }
--- Src/api-application-labmark/Migrations/20210206194709_AddTriggers.cs
+++ Src/api-application-labmark/Migrations/20210206194709_AddTriggers.cs
@@ -95,9 +95,8 @@ namespace Labmark.Migrations
 									RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
 									END CATCH;
 									END");
-			migrationBuilder.Sql($@"CREATE TRIGGER TRIGGER_CNPJ ON [LAB].[PessoaJuridica] INSTEAD OF
-									INSERT,
-										UPDATE AS BEGIN BEGIN TRY
+			migrationBuilder.Sql($@"CREATE TRIGGER TRIGGER_CNPJ_INS ON [LAB].[PessoaJuridica] INSTEAD OF
+									INSERT AS BEGIN BEGIN TRY
 									DECLARE @CNPJ varchar(20),
 										@ErrorMessage NVARCHAR(4000),
 										@ErrorSeverity INT,
@@ -125,9 +124,38 @@ namespace Labmark.Migrations
 									RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
 									END CATCH;
 									END");
-			migrationBuilder.Sql($@"CREATE TRIGGER TRIGGER_CPF ON [LAB].[PessoaFisica] INSTEAD OF
-									INSERT,
-										UPDATE AS BEGIN BEGIN TRY
+			migrationBuilder.Sql($@"CREATE TRIGGER TRIGGER_CNPJ_UPD ON [LAB].[PessoaJuridica] INSTEAD OF
+									UPDATE AS BEGIN BEGIN TRY
+									DECLARE @CNPJ varchar(20),
+										@ErrorMessage NVARCHAR(4000),
+										@ErrorSeverity INT,
+										@ErrorState INT;
+									SET NOCOUNT ON;
+									SELECT @CNPJ = CNPJ
+									FROM INSERTED IF([LAB].fncValida_CNPJ(@CNPJ) = 0) RAISERROR('CNPJ inválido.', 17, 1);
+									
+									INSERT INTO [LAB].[PessoaJuridica](
+											fkPessoaId,
+											CNPJ,
+											InscricaoEstadual,
+											ResponsavelTecnico
+										)
+									SELECT *
+									FROM INSERTED;
+									END TRY BEGIN CATCH
+									SELECT [fkPessoaId]
+									FROM [LAB].[PessoaJuridica]
+									WHERE @@ROWCOUNT = 1 AND [fkPessoaId] = @@IDENTITY
+									SELECT @ErrorMessage = ERROR_MESSAGE(),
+										@ErrorSeverity = ERROR_SEVERITY(),
+										@ErrorState = ERROR_STATE();
+									-- return the error inside the CATCH block
+									RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
+									END CATCH;
+									END");
+			migrationBuilder.Sql($@"CREATE TRIGGER TRIGGER_CPF_INS ON [LAB].[PessoaFisica] INSTEAD OF
+									INSERT
+										 AS BEGIN BEGIN TRY
 									DECLARE @CPF varchar(15),
 										@ErrorMessage NVARCHAR(4000),
 										@ErrorSeverity INT,
@@ -154,9 +182,36 @@ namespace Labmark.Migrations
 									RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
 									END CATCH;
 									END");
-			migrationBuilder.Sql($@"CREATE TRIGGER TRIGGER_PESSOA ON [LAB].[Pessoa] INSTEAD OF
-									INSERT,
-										UPDATE AS BEGIN BEGIN TRY
+			migrationBuilder.Sql($@"CREATE TRIGGER TRIGGER_CPF_UPD ON [LAB].[PessoaFisica] INSTEAD OF
+									UPDATE AS BEGIN BEGIN TRY
+									DECLARE @CPF varchar(15),
+										@ErrorMessage NVARCHAR(4000),
+										@ErrorSeverity INT,
+										@ErrorState INT;
+									SET NOCOUNT ON;
+									SELECT @CPF = CPF
+									FROM INSERTED 
+									IF([LAB].fncValida_CPF(@CPF) = 0) RAISERROR('CPF inválido.', 17, 1);
+									
+									INSERT INTO [LAB].[PessoaFisica](
+											fkPessoaId,
+											CPF
+										)
+									SELECT *
+									FROM INSERTED;
+									SELECT [fkPessoaId]
+									FROM [LAB].[PessoaFisica]
+									WHERE @@ROWCOUNT = 1 AND [fkPessoaId] = @@IDENTITY
+									END TRY BEGIN CATCH
+									SELECT @ErrorMessage = ERROR_MESSAGE(),
+										@ErrorSeverity = ERROR_SEVERITY(),
+										@ErrorState = ERROR_STATE();
+									-- return the error inside the CATCH block
+									RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
+									END CATCH;
+									END");
+			migrationBuilder.Sql($@"CREATE TRIGGER TRIGGER_PESSOA_INS ON [LAB].[Pessoa] INSTEAD OF
+									INSERT AS BEGIN BEGIN TRY
 									DECLARE @CEP varchar(8),
 										@EMAIL varchar(100),
 										@TELEFONE varchar(15),
@@ -198,7 +253,50 @@ namespace Labmark.Migrations
 									RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
 									END CATCH;
 									END");
-        }
+			migrationBuilder.Sql($@"CREATE TRIGGER TRIGGER_PESSOA_UPD ON [LAB].[Pessoa] INSTEAD OF
+									UPDATE AS BEGIN BEGIN TRY
+									DECLARE @CEP varchar(8),
+										@EMAIL varchar(100),
+										@TELEFONE varchar(15),
+										@ErrorMessage NVARCHAR(4000),
+										@ErrorSeverity INT,
+										@ErrorState INT;
+									SET NOCOUNT ON;
+									SELECT @CEP = CEP,
+										   @EMAIL = EMAIL,
+										   @TELEFONE = TELEFONE
+									FROM INSERTED 
+									IF(NOT @CEP IS NULL AND [LAB].fncVerifica_Cep(@CEP) = 0) RAISERROR('CEP inválido.', 17, 1);
+									IF(NOT @EMAIL IS NULL AND [LAB].fncValidarEmail(@EMAIL) = 0) RAISERROR('EMAIL inválido.', 17, 1);
+									IF(ISNULL(@TELEFONE,0) <> 0 AND [LAB].fncValida_Telefone(@TELEFONE) = 0) RAISERROR('TELEFONE inválido.', 17, 1);
+									
+									INSERT INTO [LAB].[Pessoa](
+										Nome,
+										Email,
+										TipoPessoa,
+										Logradouro,
+										Numero,
+										Bairro,
+										CEP,
+										TIPOACESSO,
+    									TELEFONE,
+										DDD,
+										complemento
+										)
+									SELECT INS.NOME,INS.EMAIL,INS.TIPOPESSOA,INS.LOGRADOURO,INS.NUMERO,INS.BAIRRO,INS.CEP, INS.TIPOACESSO, INS.TELEFONE, INS.DDD, ins.complemento
+									FROM INSERTED INS;
+									SELECT [Id]
+									FROM [LAB].[Pessoa]
+									WHERE @@ROWCOUNT = 1 AND [Id] = @@IDENTITY
+									END TRY BEGIN CATCH
+									SELECT @ErrorMessage = ERROR_MESSAGE(),
+										@ErrorSeverity = ERROR_SEVERITY(),
+										@ErrorState = ERROR_STATE();
+									-- return the error inside the CATCH block
+									RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
+									END CATCH;
+									END");
+		}
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
