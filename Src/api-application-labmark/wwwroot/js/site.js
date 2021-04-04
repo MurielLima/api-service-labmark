349d958dfc313c0103c82bff5ece01dbd61613f2
Muriel José Lima <murieljlima@gmail.com>
2021-04-04 20:46:43 -0300

    Fix:relatório

--- Src/api-application-labmark/wwwroot/js/site.js
+++ Src/api-application-labmark/wwwroot/js/site.js
@@ -131,3 +131,24 @@ function table(id, urlGet, urlEdit, columns, campo, exam) {
         return true;
     });
 }
+/* Valida se a data passada como parâmetro está dentro do período informado */
+function isDataValida(data, periodo) {
+    var arrayData = data.split('/');
+    var campoDia = parseInt(arrayData[]);
+    var campoMes = parseInt(arrayData[1]);
+    var campAno = parseInt(arrayData[2]);
+
+    var dataUsuario = new Date();
+    dataUsuario.setDate(campoDia);
+    dataUsuario.setMonth(campoMes - 1);
+    dataUsuario.setFullYear(campAno);
+
+    var dataLimite = new Date();
+    dataLimite.setDate(dataLimite.getDate() + periodo);
+
+    if (dataUsuario.getTime() <= dataLimite.getTime()) {
+        return true;
+    } else {
+        return false;
+    }
+}
